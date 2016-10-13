d6cc603ac81b70c1a3f59e349d5722a672f0cb32
Bigyuman <juanjo@falder.es>
2016-10-13 20:10:00 +0200

    Con fichero

--- src/main/java/Bug/pruebaGit/SumNumbersFromFile.java
+++ src/main/java/Bug/pruebaGit/SumNumbersFromFile.java
@@ -12,7 +12,7 @@ import org.apache.spark.api.java.JavaSparkContext;
  * Hello world!
  *
  */
-public class SumNumbers 
+public class SumNumbersFromFile 
 {
     public static void main( String[] args )
     {
@@ -22,15 +22,11 @@ public class SumNumbers
     	//2.-
     	JavaSparkContext sparkContext = new JavaSparkContext(conf);
 
-    	//Inicializamos con un aray que es mas cómodo
-    	Integer [] values= new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    	//pasamos a list que es como trabaja spark
-    	List<Integer> data= Arrays.asList(values);
+    	JavaRDD<String> lines = sparkContext.textFile(args[0]);
     	
-    	//3.-
-    	JavaRDD<Integer> distributedData = sparkContext.parallelize(data);
+    	JavaRDD<Integer> enteros = lines.map(s -> Integer.valueOf(s));
     	
-    	int sum = distributedData.reduce((integer1,integer2) -> (integer1 + integer2));
+    	int sum = enteros.reduce((integer1,integer2) -> (integer1 + integer2));
     	
         System.out.println( "La suma es: " + sum );
         
